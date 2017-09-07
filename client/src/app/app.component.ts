f71128c093f79c9140079623e0276c10c5e970ff
Zubko <Frank.Zubko@salientcrgt.com>
2017-09-07 09:27:31 -0400

    FEC-3 flex display and footer resize

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Committee } from './models/committee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 	isLoggedIn: boolean;
