c167578656101f1338b0282fb09ac61c8b9df330
spanyala <31629295+spanyala@users.noreply.github.com>
2017-09-06 23:07:56 -0400

    SpringBootApplication for ezamendment

---
+++ server/src/main/java/com/salientcrgt/ezamendment/EZAmendmentApplication.java
@@ -0,0 +1,12 @@
+package com.salientcrgt.ezamendment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EZAmendmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EZAmendmentApplication.class, args);
+	}
+}
