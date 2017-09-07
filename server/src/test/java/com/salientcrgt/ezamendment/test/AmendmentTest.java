b066e71043f6d9d0d825e3fef71faa9070ec694a
spanyala <31629295+spanyala@users.noreply.github.com>
2017-09-06 22:23:24 -0400

    added @SpringBootTest annotation

--- server/src/test/java/com/salientcrgt/ezamendment/test/AmendmentTest.java
+++ server/src/test/java/com/salientcrgt/ezamendment/test/AmendmentTest.java
@@ -5,6 +5,9 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -12,34 +15,38 @@ import io.restassured.http.ContentType;
 /**
  * Unit test for Amendment Process.
  */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AmendmentTest
 {
-	@BeforeClass
+    @BeforeClass
     public static void init() {
-		String port = System.getProperty("server.port");
+	String port = System.getenv("SERVER_PORT");
         
-		// If server.port not available in System property, then setting default port to be 8080
-		if (port == null) 
-            RestAssured.port = Integer.valueOf(8080);
+	// If server.port not available in System property, then setting default port to be 8080
+	if (port == null) 
+            RestAssured.port = Integer.valueOf(4200);
         else
             RestAssured.port = Integer.valueOf(port);
 		
-		// If server.base not available in System property, then setting default base to be ezamendment
-        String basePath = System.getProperty("server.base");
+	// If server.base not available in System property, then setting default base to be ezamendment
+        String basePath = System.getenv("SERVER_BASE");
         if(basePath == null)
-            basePath = "/ezamendment/";
+            basePath = "/";
         RestAssured.basePath = basePath;
-        // If server.host not available in System property, then setting default host to be localhost
-        String baseHost = System.getProperty("server.host");
+        
+	// If server.host not available in System property, then setting default host to be localhost
+        String baseHost = System.getenv("SERVER_HOST");
         if(baseHost == null)
             baseHost = "http://localhost";
         
         RestAssured.baseURI = baseHost;
     }
 	
-	@Test
+    @Test
     public void testCommitteeDetails() {
-		get("/ezamendment/committee/C00431445")
+	get("/committee/C00431445")
         .then()
         .assertThat()
         .statusCode(200)
@@ -47,8 +54,7 @@ public class AmendmentTest
         .contentType(ContentType.JSON)
 		.body("api_version", equalTo("1.0"))
 		.body("results[0].committee_id", equalTo("C00431445"))
-        .body("results[0].name", equalTo("OBAMA FOR AMERICA"));
+        	.body("results[0].name", equalTo("OBAMA FOR AMERICA"));
     }
-	
-	
+
 }
