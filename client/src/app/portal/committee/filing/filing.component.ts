2b700cbe1e9cca35ab10e88187acf2447e955c52
Zubko <Frank.Zubko@salientcrgt.com>
2017-09-08 19:24:36 -0400

    FEC-13 fa integration and datepicker draft

--- client/src/app/portal/committee/filing/filing.component.ts
+++ client/src/app/portal/committee/filing/filing.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilingComponent implements OnInit {
 
+  filing = {};
+  
   constructor() { }
 
   ngOnInit() {
--- client/src/app/portal/committee/filings/filings.component.ts
+++ client/src/app/portal/committee/filings/filings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-filings',
