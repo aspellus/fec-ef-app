34b56a30e1c28b38a33789be2cede676eb74eba3
Zubko <Frank.Zubko@salientcrgt.com>
2017-09-08 15:21:37 -0400

    FEC-13 committee dropdown

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
+import { CommitteeService } from "./portal/committee/committee.service";
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,24 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
 	isLoggedIn: boolean;
-
-	constructor(private router: Router){
+	committeeIds: String[] = ['C00577759','C00579599','C00615740','C00634634'];
+	committees: any[] = [];
+	
+	constructor(private router: Router, private committeeService: CommitteeService){
 		this.isLoggedIn = false;
 	}
 
+	ngOnInit(){
+		this.committeeIds.forEach(function forEach(committeeId){
+			this.committeeService.getOne(committeeId).subscribe(
+				data => {
+					this.committees.push(data.results[0]);
+				},
+				error => console.log(error)
+			);
+		}.bind(this));
+	}
+	
 	committeeLogin(id: string){
 		this.isLoggedIn = true;
 		this.router.navigate(['/portal/committee/' + id]);
