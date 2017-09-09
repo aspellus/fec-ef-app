83cd8570c1d96fb86c35f2e8bcba33d47a58f780
spanyala <31629295+spanyala@users.noreply.github.com>
2017-09-09 10:21:42 -0400

    FEC-40, creates a localhost postgresql datasource

---
+++ server/src/main/java/com/salientcrgt/ezamendment/config/root/ApplicationConfiguration.java
@@ -0,0 +1,63 @@
+package com.salientcrgt.ezamendment.config.root;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Development specific configuration - creates a localhost postgresql datasource,
+ * sets hibernate on create drop mode and inserts some test data on the database.
+ * <p>
+ * Set -Dspring.profiles.active=development to activate this config.
+ */
+@Configuration
+//@Profile("development")
+@EnableTransactionManagement
+public class ApplicationConfiguration {
+
+    @Bean(name = "datasource")
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:" + System.getenv("EF_DB_URL"));
+        dataSource.setUsername(System.getenv("EF_DB_USERNAME"));
+        dataSource.setPassword(System.getenv("EF_DB_PASSWORD"));
+        if(System.getenv("EF_DB_URL") == null) 
+        	dataSource.setUrl("jdbc:postgresql://localhost:5432/fecefapp");
+        if(System.getenv("EF_DB_USERNAME") == null) 
+        	dataSource.setUsername("fecefapp");
+        if(System.getenv("EF_DB_PASSWORD") == null) 
+        	dataSource.setPassword("FECEF01");
+        
+        return dataSource;
+    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DriverManagerDataSource dataSource) {
+
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setPackagesToScan(new String[]{"com.salientcrgt.ezamendment.model"});
+        entityManagerFactoryBean.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Map<String, Object> jpaProperties = new HashMap<String, Object>();
+        jpaProperties.put("hibernate.hbm2ddl.auto", "update");
+        jpaProperties.put("hibernate.show_sql", "true");
+        jpaProperties.put("hibernate.format_sql", "true");
+        jpaProperties.put("hibernate.use_sql_comments", "true");
+        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        entityManagerFactoryBean.setJpaPropertyMap(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
+
+}
