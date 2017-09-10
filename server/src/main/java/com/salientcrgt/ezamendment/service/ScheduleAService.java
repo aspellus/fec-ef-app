4c4502731acce3f88f634747acf75466cbc855a2
spanyala <31629295+spanyala@users.noreply.github.com>
2017-09-10 13:57:04 -0400

    FEC-23, Changes for Add/Update/Delete REST API

--- server/src/main/java/com/salientcrgt/ezamendment/service/ScheduleAService.java
+++ server/src/main/java/com/salientcrgt/ezamendment/service/ScheduleAService.java
@@ -1,7 +1,7 @@
 package com.salientcrgt.ezamendment.service;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.salientcrgt.ezamendment.controller.AmendmentController;
 import com.salientcrgt.ezamendment.dao.ScheduleARepository;
+import com.salientcrgt.ezamendment.dto.ScheduleADTO;
 import com.salientcrgt.ezamendment.model.ScheduleA;
 
 /**
@@ -33,39 +34,47 @@ public class ScheduleAService {
     
     /**
     *
-    * merges a new ScheduleA in the database
+    * merges ScheduleA in the database
     *
     */
-   @Transactional
-   public ScheduleA mergeScheduleA(long report_id, String committee_id, String line_num, String tran_id, String lname, String fname, String mname, 
-								String prefix, String suffix, String str1, String str2, String city, String state, String zip, Date date_con, 
-								double amount, String transdesc, String indemp, String indocc, String memo_code) {
-		ScheduleA scheduleA = new ScheduleA(report_id, line_num, 0L, committee_id, tran_id, "", lname, fname, mname, 
-				   								prefix, suffix, str1, str2, city, state, zip, "", "", date_con, 
-				   								amount, 0d, transdesc, indemp, indocc, memo_code);
-		scheduleA = scheduleARepository.merge(scheduleA);
-		return scheduleA;
-   }
+    @Transactional
+    public ScheduleA mergeScheduleA(long report_id, ScheduleADTO scheduleADTO) {
+ 		ScheduleA scheduleA = new ScheduleA(scheduleADTO.getRepid(), scheduleADTO.getLine_num(), 0L, scheduleADTO.getComid(), scheduleADTO.getTran_id(), "", 
+												scheduleADTO.getName(), scheduleADTO.getFname(), scheduleADTO.getMname(),scheduleADTO.getPrefix(), 
+												scheduleADTO.getSuffix(), scheduleADTO.getStr1(), scheduleADTO.getStr2(), scheduleADTO.getCity(), scheduleADTO.getState(), 
+												scheduleADTO.getZip(), "", "", scheduleADTO.getDate_con(), scheduleADTO.getAmount(), scheduleADTO.getYtd(), 
+												scheduleADTO.getTransdesc(), scheduleADTO.getIndemp(), scheduleADTO.getIndocc(), scheduleADTO.getMemo_code(), "C");
+ 		scheduleA = scheduleARepository.merge(scheduleA);
+ 		return scheduleA;
+    } 
+    
    
    /**
    *
    * creates a new ScheduleA in the database
    *
    */
-  @Transactional
-  public ScheduleA createScheduleA(long report_id, String committee_id, String line_num, String tran_id, String lname, String fname, String mname, 
-								String prefix, String suffix, String str1, String str2, String city, String state, String zip, Date date_con, 
-								double amount, String transdesc, String indemp, String indocc, String memo_code) {
-		ScheduleA scheduleA = new ScheduleA(report_id, line_num, 0L, committee_id, tran_id, "", lname, fname, mname, 
-				   								prefix, suffix, str1, str2, city, state, zip, "", "", date_con, 
-				   								amount, 0d, transdesc, indemp, indocc, memo_code);
-		scheduleA = scheduleARepository.create(scheduleA);
-		return scheduleA;
-  }
-  
+   @Transactional
+   public ScheduleA createScheduleA(long report_id, ScheduleADTO scheduleADTO) {
+	   	if(scheduleADTO.getTran_id() != null || !scheduleADTO.getTran_id().trim().equals("")) {
+	   		//Tran ID has been generated from Front End 
+	   	} else {
+	   		//assuming it's empty and generating Tran ID
+	   		long randomNumber = new Random().nextLong();
+	   		scheduleADTO.setTran_id("SA-"+randomNumber);
+	   	}
+ 		ScheduleA scheduleA = new ScheduleA(scheduleADTO.getRepid(), scheduleADTO.getLine_num(), 0L, scheduleADTO.getComid(), scheduleADTO.getTran_id(), "", 
+ 												scheduleADTO.getName(), scheduleADTO.getFname(), scheduleADTO.getMname(),scheduleADTO.getPrefix(), 
+ 												scheduleADTO.getSuffix(), scheduleADTO.getStr1(), scheduleADTO.getStr2(), scheduleADTO.getCity(), scheduleADTO.getState(), 
+ 												scheduleADTO.getZip(), "", "", scheduleADTO.getDate_con(), scheduleADTO.getAmount(), scheduleADTO.getYtd(), 
+ 												scheduleADTO.getTransdesc(), scheduleADTO.getIndemp(), scheduleADTO.getIndocc(), scheduleADTO.getMemo_code(), "A");
+ 		scheduleA = scheduleARepository.create(scheduleA);
+ 		return scheduleA;
+   } 
+   
   /**
   *
-  * creates a new ScheduleA in the database
+  * deletes ScheduleA from the database
   *
   */
   @Transactional
