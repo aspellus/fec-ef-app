b0710c5f246dde45a97328ed00398e7523cc409c
Zubko <Frank.Zubko@salientcrgt.com>
2017-09-09 23:43:52 -0400

    FEC-22 button types to prevent form submit, blank receipt for new and default sorting

--- client/src/app/portal/committee/filing/filing.component.ts
+++ client/src/app/portal/committee/filing/filing.component.ts
@@ -12,12 +12,13 @@ export class FilingComponent implements OnInit {
 
   filing = {"fec_url":"http://docquery.fec.gov/dcdev/posted/1178702.fec","opposition_personal_funds":null,"amendment_chain":[1178702],"total_communication_cost":null,"pdf_url":"http://docquery.fec.gov/pdf/692/201708119070320692/201708119070320692.pdf","senate_personal_funds":null,"form_type":"F3","office":null,"pages":4,"is_amended":false,"primary_general_indicator":"P","candidate_name":null,"state":null,"most_recent":true,"sub_id":"4081120171446512792","amendment_indicator":"N","cash_on_hand_beginning_period":0,"beginning_image_number":"201708119070320692","fec_file_id":"FEC-1178702","report_type":"Q2","update_date":"2017-08-11T21:12:34","debts_owed_to_committee":0,"ending_image_number":"201708119070320695","html_url":"http://docquery.fec.gov/cgi-bin/forms/C00577759/1178702/","report_year":2017,"election_year":null,"party":null,"committee_id":"C00577759","coverage_start_date":"2017-04-01T00:00:00","csv_url":"http://docquery.fec.gov/csv/702/1178702.csv","request_type":null,"previous_file_number":1178702,"house_personal_funds":null,"document_description":"JULY QUARTERLY 2017","document_type_full":null,"treasurer_name":null,"cycle":2018,"total_independent_expenditures":null,"total_disbursements":0,"candidate_id":null,"file_number":1178702,"committee_name":"UBERPAC","net_donations":null,"means_filed":"e-file","amendment_version":0,"document_type":null,"total_individual_contributions":null,"report_type_full":"JULY QUARTERLY","most_recent_file_number":1178702,"cash_on_hand_end_period":0,"total_receipts":0,"coverage_end_date":"2017-06-30T00:00:00","receipt_date":"2017-08-11T00:00:00","debts_owed_by_committee":0};
   report_id: number;
-  receipts: Array<any>;
+  receipts: Array<any> = [];
   lineNums: Array<any>;
   
   constructor(private router: Router, private route: ActivatedRoute, private filingService: FilingService) { }
 
   ngOnInit() {
+	  console.log('init');
 	  this.lineNums = [{id: '11A', desc: 'Individual Contribution'},
 	                    {id: '11B', desc: 'Political Party Contribution'},
 	                    {id: '11C', desc: 'Political Committee (Other) Contribution'},
@@ -32,7 +33,32 @@ export class FilingComponent implements OnInit {
 		 this.report_id = params['file_id']; 
 	  });
 	  this.filingService.getReceipts(this.report_id).subscribe(data => {
+		  data.sort(function(receipt_a, receipt_b){
+		  	var line_num_a = receipt_a.line_num.toUpperCase(),
+		  		line_num_b = receipt_a.line_num.toUpperCase(),
+		  		date_a = new Date(receipt_a.date_con),
+		  		date_b = new Date(receipt_b.date_con),
+		  		name_a = (receipt_a.name + receipt_a.fname).toUpperCase(),
+		  		name_b = (receipt_b.name + receipt_b.fname).toUpperCase();
+		  	
+		  	if (line_num_a < line_num_b) { return -1; }
+		  	if (line_num_a > line_num_b) { return 1; }
+		  	if (date_a < date_b) { return -1; }
+		  	if (date_a > date_b) { return 1; }
+		  	if (name_a < name_b) { return -1; }
+		  	if (name_a > name_b) { return 1; }
+		  	if (receipt_a.amount < receipt_b.amount) { return -1; }
+		  	if (receipt_a.amount > receipt_b.amount) { return 1; }
+		  	
+		  	//console.log(receipt);
+		  	return 0;
+		  });
 		  this.receipts = data;
 	  });  
   }
+  
+	getSortedReceipts(){
+		console.log('get');
+		return this.receipts;
+	}
 }
