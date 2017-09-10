85d61f3fb6e428351b94a0e6cecd00078457107a
spanyala <31629295+spanyala@users.noreply.github.com>
2017-09-09 20:16:42 -0400

    FEC23 - Receipts Create, Update, Detele

--- server/src/main/java/com/salientcrgt/ezamendment/service/ScheduleAService.java
+++ server/src/main/java/com/salientcrgt/ezamendment/service/ScheduleAService.java
@@ -1,5 +1,6 @@
 package com.salientcrgt.ezamendment.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -29,5 +30,47 @@ public class ScheduleAService {
     public List<ScheduleA> findByReportId(long reportId) {
         return scheduleARepository.findByReportId(reportId);
     }
+    
+    /**
+    *
+    * merges a new ScheduleA in the database
+    *
+    */
+   @Transactional
+   public ScheduleA mergeScheduleA(long report_id, String committee_id, String line_num, String tran_id, String lname, String fname, String mname, 
+								String prefix, String suffix, String str1, String str2, String city, String state, String zip, Date date_con, 
+								double amount, String transdesc, String indemp, String indocc, String memo_code) {
+		ScheduleA scheduleA = new ScheduleA(report_id, line_num, 0L, committee_id, tran_id, "", lname, fname, mname, 
+				   								prefix, suffix, str1, str2, city, state, zip, "", "", date_con, 
+				   								amount, 0d, transdesc, indemp, indocc, memo_code);
+		scheduleA = scheduleARepository.merge(scheduleA);
+		return scheduleA;
+   }
+   
+   /**
+   *
+   * creates a new ScheduleA in the database
+   *
+   */
+  @Transactional
+  public ScheduleA createScheduleA(long report_id, String committee_id, String line_num, String tran_id, String lname, String fname, String mname, 
+								String prefix, String suffix, String str1, String str2, String city, String state, String zip, Date date_con, 
+								double amount, String transdesc, String indemp, String indocc, String memo_code) {
+		ScheduleA scheduleA = new ScheduleA(report_id, line_num, 0L, committee_id, tran_id, "", lname, fname, mname, 
+				   								prefix, suffix, str1, str2, city, state, zip, "", "", date_con, 
+				   								amount, 0d, transdesc, indemp, indocc, memo_code);
+		scheduleA = scheduleARepository.create(scheduleA);
+		return scheduleA;
+  }
+  
+  /**
+  *
+  * creates a new ScheduleA in the database
+  *
+  */
+  @Transactional
+  public void deleteScheduleA(long report_id, String tran_id) {
+		scheduleARepository.delete(report_id, tran_id);
+  }
 
 }
