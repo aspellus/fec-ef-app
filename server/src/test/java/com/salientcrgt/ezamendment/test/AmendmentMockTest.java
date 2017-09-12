d193b9b53156eec088ec270b20a53a7a0fc6b952
tuxotron <rafael@troncoso.org>
2017-09-12 19:33:36 -0400

    fixed tests

--- server/src/test/java/com/salientcrgt/ezamendment/test/AmendmentMockTest.java
+++ server/src/test/java/com/salientcrgt/ezamendment/test/AmendmentMockTest.java
@@ -1,19 +1,10 @@
 package com.salientcrgt.ezamendment.test;
 
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyLong;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.salientcrgt.ezamendment.controller.AmendmentController;
 import com.salientcrgt.ezamendment.dto.ScheduleADTO;
 import com.salientcrgt.ezamendment.model.ScheduleA;
-import org.junit.Ignore;
+import com.salientcrgt.ezamendment.service.ScheduleAService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +14,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.salientcrgt.ezamendment.controller.AmendmentController;
-import com.salientcrgt.ezamendment.service.ScheduleAService;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
@@ -45,7 +41,7 @@ public class AmendmentMockTest {
                 .andExpect(status().isOk());
 
     }
-    
+
     @Test
     public void shouldReturnCommiteeFilingDetails() throws Exception {
         this.mockMvc
@@ -54,7 +50,7 @@ public class AmendmentMockTest {
                 .andExpect(status().isOk());
 
     }
-    
+
     @Test
     public void shouldReturnCommiteeFilingDetailsByFormType() throws Exception {
         this.mockMvc
@@ -63,7 +59,7 @@ public class AmendmentMockTest {
                 .andExpect(status().isOk());
 
     }
-    
+
     @Test
     public void shouldReturnReceipts() throws Exception {
         this.mockMvc
@@ -72,43 +68,32 @@ public class AmendmentMockTest {
                 .andExpect(status().isOk());
 
     }
-    
+
     @Test
     public void shouldCreateReceipt() throws Exception {
         when(service.createScheduleA(anyLong(), any(ScheduleADTO.class))).thenReturn(new ScheduleA());
         this.mockMvc
                 .perform(post("/schedules/1178702/schedule_a/")
-                .content("{\"comid\": \"C00577759\"}")
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                        .content("{\"comid\": \"C00577759\"}")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andDo(print())
                 .andExpect(status().isOk());
 
     }
-    
-    @Test @Ignore
+
+    @Test
     public void shouldUpdateReceipt() throws Exception {
+        when(service.mergeScheduleA(anyLong(), anyString(), any(ScheduleADTO.class))).thenReturn(new ScheduleA());
+
         this.mockMvc
                 .perform(put("/schedules/1178702/schedule_a/SA11AI.26847/?")
-                		.param("comid", "C00577759")
-                    	.param("line_num", "11AI")
-                    	.param("name", "Gardner")
-                    	.param("fname", "Drew")
-                    	.param("str1", "8255 Greensboro Dr")
-                    	.param("str2", "Suite 400")
-                    	.param("city", "Tysons")
-                    	.param("state", "VA")
-                    	.param("zip", "22102")
-                    	.param("date_con", "10/14/2016")
-                    	.param("amount", "3000")
-                    	.param("transdesc", "Testing Updating a Receipt")
-                    	.param("indemp", "SalientCRGT")
-                    	.param("indocc", "")
-                    	.param("memo_code", ""))
+                        .content("{\"comid\": \"C00577759\"}")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andDo(print())
                 .andExpect(status().isOk());
 
     }
-    
+
     @Test
     public void shouldDeleteReceipt() throws Exception {
         this.mockMvc
